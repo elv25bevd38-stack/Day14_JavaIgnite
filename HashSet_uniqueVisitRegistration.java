74419abce44b3ee1341d7693b688b5bef8b6dcb6
rite2harini <rite2harini@gmail.com>
2026-06-10 15:07:44 +0530

    Create HashSet_uniqueVisitRegistration.java

---
+++ HashSet_uniqueVisitRegistration.java
@@ -0,0 +1,18 @@
+/*
+Unique Visitor Registration
+
+A college event registration system should not allow duplicate registrations.
+
+Requirements:
+Store participant names using a HashSet.
+Add at least 8 names.
+Intentionally add a few duplicate names.
+Display all registered participants.
+Display the total number of unique participants.
+Hints
+HashSet
+add()
+size()
+Duplicate removal behavior
+
+*/
