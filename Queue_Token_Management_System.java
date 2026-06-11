28c89835ac12c5c3c158f95b5f91dddad5f75ce7
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-11 09:34:58 +0530

    Remove duplicate patient queue logic

--- Queue_Token_Management_System.java
+++ Queue_Token_Management_System.java
@@ -45,11 +45,12 @@ public class Token_Management_System {
 		Queue<String> patientQueue = new LinkedList<>();
 		//Adding patients in queue using offer()
 		
-		patientQueue.offer("Nandini");
+		
 		patientQueue.offer("Ayush");
 		patientQueue.offer("Asmit");
 		patientQueue.offer("Sanjeeb");
 		patientQueue.offer("Supriya");
+		patientQueue.offer("Nandini");
 		
 		System.out.println("Patients in the queue order: ");
 		int i=1;
@@ -76,88 +77,3 @@ public class Token_Management_System {
 	}
 
 }
-import java.util.*;
-
-public class Token_Management_System {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Generating queue named patientQueue
-		
-		Queue<String> patientQueue = new LinkedList<>();
-		//Adding patients in queue using offer()
-		
-		patientQueue.offer("Nandini");
-		patientQueue.offer("Ayush");
-		patientQueue.offer("Asmit");
-		patientQueue.offer("Sanjeeb");
-		patientQueue.offer("Supriya");
-		
-		System.out.println("Patients in the queue order: ");
-		int i=1;
-		for(String name:patientQueue)
-		{
-			System.out.println(i+"."+name);
-			i++;
-		}
-		
-		//Peeking the first patient and then adding it to servedPatient
-		System.out.println("Serving the first patient: "+patientQueue.peek());
-		String servedPatient = patientQueue.poll();
-		System.out.println("Served patients: "+servedPatient);
-		
-		//Remaining patients in the patientQueue
-		System.out.println("Remaining patients in queue:");
-		i=1;
-		for(String name:patientQueue)
-		{
-			System.out.println(i+"."+name);
-			i++;
-		}
-		
-	}
-
-}
-import java.util.*;
-
-public class Token_Management_System {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Generating queue named patientQueue
-		
-		Queue<String> patientQueue = new LinkedList<>();
-		//Adding patients in queue using offer()
-		
-		patientQueue.offer("Nandini");
-		patientQueue.offer("Ayush");
-		patientQueue.offer("Asmit");
-		patientQueue.offer("Sanjeeb");
-		patientQueue.offer("Supriya");
-		
-		System.out.println("Patients in the queue order: ");
-		int i=1;
-		for(String name:patientQueue)
-		{
-			System.out.println(i+"."+name);
-			i++;
-		}
-		
-		//Peeking the first patient and then adding it to servedPatient
-		System.out.println("Serving the first patient: "+patientQueue.peek());
-		String servedPatient = patientQueue.poll();
-		System.out.println("Served patients: "+servedPatient);
-		
-		//Remaining patients in the patientQueue
-		System.out.println("Remaining patients in queue:");
-		i=1;
-		for(String name:patientQueue)
-		{
-			System.out.println(i+"."+name);
-			i++;
-		}
-		
-	}
-
-}
-
